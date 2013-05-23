635c33a8953094328e069c164a554ba04b90f193
Aaron Patterson <aaron.patterson@gmail.com>
2013-05-23 10:06:30 -0700

    each returns self so we can just mutate

--- lib/hana.rb
+++ lib/hana.rb
@@ -30,8 +30,8 @@ module Hana
     def self.parse path
       return [''] if path == '/'
 
-      path.sub(/^\//, '').split(/(?<!\^)\//).map! { |part|
-        part.gsub!(/\^[\/^]|~[01]/) { |m| ESC[m] }; part
+      path.sub(/^\//, '').split(/(?<!\^)\//).each { |part|
+        part.gsub!(/\^[\/^]|~[01]/) { |m| ESC[m] }
       }
     end
   end
